18a0cb887015cb8bdd4685350508e142d24b0a76
meli99b <Melissa.Bastas@Student.HTW-Berlin.de>
2026-04-10 10:28:05 +0200

    AnfangProjekt

---
+++ src/main/java/htw/webtech/webtech26/Webtech26Application.java
@@ -0,0 +1,13 @@
+package htw.webtech.webtech26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webtech26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webtech26Application.class, args);
+	}
+
+}
---
+++ src/test/java/htw/webtech/webtech26/Webtech26ApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.webtech.webtech26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webtech26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
